85db19d3cb0f9c9f2939647a9399878e3c2d8cb8
amitabhisgood <4756949+amitabhisgood@users.noreply.github.com>
2024-02-20 14:51:06 +0530

    Update LandingPage.js

--- LandingPage.js
+++ LandingPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from './header';
-import FCenterComponent from './fCenterComponent';
+import Header from './Header';
+import FCenterComponent from './FCenterComponent';
 
 function LandingPage() {
   return (
