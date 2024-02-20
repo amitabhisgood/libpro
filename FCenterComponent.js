6a9ff0c61da061992750438b9de191c1d1f66629
amitabhisgood <4756949+amitabhisgood@users.noreply.github.com>
2024-02-20 14:43:40 +0530

    Update FCenterComponent.js

--- FCenterComponent.js
+++ FCenterComponent.js
@@ -1,7 +1,18 @@
 const { useState } = React;
 
 function FCenterComponent() {
-  // ... (Same as your existing FCenterComponent component)
+  const [showRegisterForm, setShowRegisterForm] = useState(false);
+  
+  return (
+    <div className="f-center-component">
+      <div className="left-component">
+        <LeftComponent />
+      </div>
+      <div className="center-component">
+        <CenterComponent showRegisterForm={showRegisterForm} setShowRegisterForm={setShowRegisterForm} />
+      </div>
+    </div>
+  );
 }
 
 export default FCenterComponent;
