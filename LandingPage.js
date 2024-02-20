d29bed497961cafa9d9344917121297e868430fa
amitabhisgood <4756949+amitabhisgood@users.noreply.github.com>
2024-02-20 14:35:52 +0530

    Create LandingPage.js

---
+++ LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './header';
+import FCenterComponent from './fCenterComponent';
+
+function LandingPage() {
+  return (
+    <div>
+      <Header />
+      <FCenterComponent />
+    </div>
+  );
+}
+
+export default LandingPage;
