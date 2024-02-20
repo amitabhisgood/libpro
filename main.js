4c1c92be95dcab13ac725b84943a750484399d65
amitabhisgood <4756949+amitabhisgood@users.noreply.github.com>
2024-02-20 15:10:21 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import FCenterComponent from './FCenterComponent';
+import LandingPage from './LandingPage.js';
+
 
 ReactDOM.render(<LandingPage />, document.getElementById('root'));
